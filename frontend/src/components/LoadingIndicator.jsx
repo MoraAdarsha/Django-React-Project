a3f32c3d6936ed6bff25c44f68da34f1fe0546d9
Adarsha Mora <moraadarsha@gmail.com>
2025-12-17 14:53:34 +0530

    Initial commit: Django + React project

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,8 @@
+import "../styles/LoadingIndicator.css"
+
+const LoadingIndicator = () => {
+    return <div className ="loading-indicator">
+        <div className="spinner"></div>
+    </div>
+}
+export default LoadingIndicator
